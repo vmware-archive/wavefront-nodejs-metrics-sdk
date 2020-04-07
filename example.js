7e5e85b86eb9456f47bafda3c4c666f3203142d3
arielyu <yzheqing@vmware.com>
2020-04-07 15:51:48 -0700

    Create example for metric reporting

---
+++ example.js
@@ -0,0 +1,50 @@
+const TaggedRegistry = require('./src/registry'),
+  WavefrontReporter = require('./src/wavefront-reporter'),
+  wavefrontHistogram = require('./src/wavefrontHistogram');
+
+// Metrics Reporting Function Example.
+function reportMetrics(server, token) {
+  // Create tagged registry and reporter
+  let reg = new TaggedRegistry();
+  let wfReporter = new WavefrontReporter({
+    server: server,
+    token: token,
+    source: 'wavefront-javascript-metrics',
+    registry: reg,
+    tags: { key1: 'val1', key2: 'val2' }
+  }).reportMinuteDistribution();
+  wfReporter.start(5000);
+
+  // counter
+  let c = reg.counter('request.counter', { key1: 'val1' });
+  c.inc();
+
+  // gauge
+  let g = reg.gauge('request.gauge', () => 2 + Math.random(), {
+    key1: 'val1'
+  });
+
+  // histogram
+  let h = reg.histogram('request.duration.foo', { key1: 'val1' });
+  h.update(50);
+
+  // wavefront histogram
+  let wf_h = wavefrontHistogram.wavefrontHistogram(reg, 'request.duration.wf', {
+    key1: 'val1'
+  });
+  wf_h.update(1.0);
+  wf_h.update(1.0);
+
+  // meter
+  let m = reg.meter('request.meter', { key1: 'val1' });
+  m.mark(1);
+
+  // timer
+  let t = reg.timer('request.timer', { key1: 'val1' });
+  t.update(50);
+
+  // stop the reporter
+  wfReporter.stop();
+}
+
+reportMetrics('test_server', 'test_token');
